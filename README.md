58520dadd287c7fec6efb373d125cc72900b5ed9
Chiara Fusar Bassini <c.fusarbassini@hertie-school.org>
2024-10-30 22:05:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Problem-Set-3
+
+Problem set 3 of the course Deep Learning WS 2023/2024. 
+Tasks are indicated with the #TODO flag. In case of problems, open an issue on your repository and take the TA with @chiara-fb.
+Remember to show Notebook output when uploading your code to avoid penalization.
+
+- Add a stemming method to a vocabulary class
+- Implement an attention layer
+- Load two pretrained translation models and fine-tune them on your data.
---
+++ utils.py
@@ -0,0 +1,47 @@
+import time
+import math
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import unicodedata
+import re
+plt.switch_backend('agg')
+
+
+# https://stackoverflow.com/a/518232/2809427
+def unicode_to_ascii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# Lowercase, trim, and remove non-letter characters
+def normalize_string(s):
+    s = unicode_to_ascii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+def showPlot(points):
+    plt.figure()
+    fig, ax = plt.subplots()
+    # this locator puts ticks at regular intervals
+    loc = ticker.MultipleLocator(base=0.2)
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
+    return fig
